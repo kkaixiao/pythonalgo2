0937f9fa1fa236d0b8890dc0c1104927c6910ee4
kkaixiao <show-key@qq.com>
2019-11-27 23:13:39 +0800

    1st commit, while watching the video

---
+++ stack_class.py
@@ -0,0 +1,36 @@
+
+class Stack(object):
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+
+
+s = Stack()
+
+print(s.isEmpty())
+
+s.push(1)
+
+print(s.isEmpty())
+
+print(s.size())
+
+s.pop()
+
+print(s.isEmpty())
