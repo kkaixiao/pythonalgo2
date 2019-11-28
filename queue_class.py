96077b35cb72937e13ac1ca81aaa960774db3012
kkaixiao <show-key@qq.com>
2019-11-28 21:35:34 +0800

    1st commit, after watching the video

---
+++ queue_class.py
@@ -0,0 +1,36 @@
+
+class Queue(object):
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enque(self, item):
+        self.items.insert(0, item)
+
+    def deque(self):
+        self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
+q = Queue()
+print(q.size())
+
+print(q.isEmpty())
+
+q.enque(1)
+q.enque(2)
+print(q.isEmpty())
+print(q.size())
+
+q.deque()
+print(q.size())
+
+q.deque()
+print(q.size())
+
+print(q.isEmpty())
