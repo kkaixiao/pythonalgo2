f22d0a95cae8f85d62ce0e72298480d009928066
kkaixiao <show-key@qq.com>
2019-12-04 15:01:47 +0800

    1st commit, after answer video

---
+++ recursion_hm3_word_split.py
@@ -0,0 +1,16 @@
+
+def word_split(phrase, list_of_words, output=None):
+    if output is None:
+        output = []
+
+    for word in list_of_words:
+        if phrase.startswith(word):
+            output.append(word)
+            return word_split(phrase[len(word):], list_of_words, output)
+
+    return output
+
+
+# print(word_split('themanran', ['the', 'ran', 'man']))
+
+print(word_split('ilovedogsJohn', ['i', 'am', 'a', 'dogs', 'lover', 'love', 'John']))
