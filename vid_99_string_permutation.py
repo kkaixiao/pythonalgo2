4c55bc5b3d705a592ff5cedb017a6a036910854b
kkaixiao <show-key@qq.com>
2019-12-08 22:40:45 +0800

    1st commit after video

---
+++ vid_99_string_permutation.py
@@ -0,0 +1,19 @@
+
+def permute(s):
+    out = []
+
+    # Base Case
+    if len(s) == 1:
+        out = [s]
+
+    else:
+    # for every letter in the string s
+        for i, let in enumerate(s):
+
+            for perm in permute(s[:i] + s[i+1:]):
+                out += [let + perm]
+
+    return out
+
+
+print(permute('abc'))
