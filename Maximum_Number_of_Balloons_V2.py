33f5cee70ed4999617526419523d6b36d2ef3d85
kkaixiao <show-key@qq.com>
2019-12-09 23:57:07 +0800

    3rd commit non-recursion before tutorial

--- Maximum_Number_of_Balloons_V2.py
+++ Maximum_Number_of_Balloons_V2.py
@@ -3,25 +3,22 @@
 # recursion version of max number of balloons
 # I think there's something wrong, but at least the whole logic is built
 
-def maxNumberOfBalloons(text_list, balloon_str='balloon', count = 0):
+def maxNumberOfBalloons(text_list, balloon_str='balloon', count=0):
     if len(text_list) < len(balloon_str):
         return count
 
     balloon_list_check = list(balloon_str)
-    for i in range(len(text_list)):
-        char = text_list[i]
-        try:
-            char_index = balloon_list_check.index(char)
-        except ValueError:
-            char_index = -1
-        if char_index >= 0:
-            balloon_list_check.pop(char_index)
-            if len(balloon_list_check) == 0:
 
-                return maxNumberOfBalloons(text_list[i:], 'balloon', count+1)
+    for i, char in enumerate(balloon_str):
+        balloon_list_check.remove(char)
+        if char in text_list:
+            text_list.remove(char)
+
+        if len(balloon_list_check) == 0:
+            return maxNumberOfBalloons(text_list, 'balloon', count+1)
 
 
 
 
 
-print(maxNumberOfBalloons('loonbalbalballoonlodfoballoballoondfonndballoofddn'))
+print(maxNumberOfBalloons(list('bsdfdballnooaoolln')))
