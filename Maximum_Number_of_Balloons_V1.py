1ff31cc9945ff9aca080500852028f478cbb357d
kkaixiao <show-key@qq.com>
2019-12-09 19:00:56 +0800

    1st commit non-recursion before tutorial

---
+++ Maximum_Number_of_Balloons_V1.py
@@ -0,0 +1,25 @@
+def maxNumberOfBalloons(text, balloon_str='balloon'):
+    balloon_list_check = list(balloon_str)
+    count = 0
+
+    for i, char in enumerate(text):
+        if char in balloon_list_check:
+            balloon_list_check.remove(char)
+
+        if len(balloon_list_check) == 0:
+            count += 1
+            balloon_list_check = list(balloon_str)
+
+    return count
+
+
+def cleanString(text, balloon_str='balloon'):
+    balloon_list = balloon_str
+    text_list = list(text)
+    for char in text_list:
+        if char not in balloon_list:
+            text_list.remove(char)
+
+    return str(text_list)
+
+print(maxNumberOfBalloons(cleanString('loonbalxballpoon')))
