d69feb1ab190124bcdf4c51f289248f2207c6f05
kkaixiao <show-key@qq.com>
2019-12-10 11:28:59 +0800

    1st commit: the simplest recursion version before tutorial

---
+++ vid_101_fibonacci_v1.py
@@ -0,0 +1,23 @@
+
+# The Fibonacci Sequence is the series of numbers:
+#
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+#
+# The next number is found by adding up the two numbers before it.
+#
+# The 2 is found by adding the two numbers before it (1+1)
+# The 3 is found by adding the two numbers before it (1+2),
+# And the 5 is (2+3),
+# and so on!
+
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n <= 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+print(fibonacci(9))
