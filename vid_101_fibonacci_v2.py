d4c244be0d430ba714eef695cae1e27f0c056708
kkaixiao <show-key@qq.com>
2019-12-10 13:53:49 +0800

    1st commit, an implementation with recursion+memoization approach, after watching tutorial

--- vid_101_fibonacci_v2.py
+++ vid_101_fibonacci_v2.py
@@ -7,15 +7,30 @@
 # And the 5 is (2+3),
 # and so on!
 
-# we use memoization technique to accelerate computing
+# we use memoization technique to accelerate computing.
+
+# cache, consider setting n to a number bigger than function call input n
+n = 100
+cache = [None] * (n+1)
+
 
 def fibonacci(n):
+
+    # base case
     if n <= 0:
         return 0
     elif n <= 2:
         return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+
+    # check cache
+
+    if cache[n]:
+        return cache[n]
+
+    # keep setting cache
+    cache[n] = fibonacci(n-1) + fibonacci(n-2)
+
+    return cache[n]
 
 
-print(fibonacci(10))
+print(fibonacci(20))
