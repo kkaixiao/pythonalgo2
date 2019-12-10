b7d3ad4ed02ae774797558dbfd8fa09f3c5b4ce1
kkaixiao <show-key@qq.com>
2019-12-10 14:56:49 +0800

    2ne commit, added python 3.2+ built in function of automatic caching

--- vid_101_fibonacci_v2.py
+++ vid_101_fibonacci_v2.py
@@ -10,6 +10,8 @@
 # we use memoization technique to accelerate computing.
 
 # cache, consider setting n to a number bigger than function call input n
+
+# the ordinary memoization
 n = 100
 cache = [None] * (n+1)
 
@@ -28,9 +30,24 @@ def fibonacci(n):
         return cache[n]
 
     # keep setting cache
+    # cache.append(fibonacci(n-1) + fibonacci(n-2))
     cache[n] = fibonacci(n-1) + fibonacci(n-2)
 
     return cache[n]
 
 
-print(fibonacci(20))
+# print(fibonacci(50))
+
+## end of the ordinary memoization
+
+
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fib_cache_built_in(n):
+    if n < 2:  # base case
+        return n
+    return fib_cache_built_in(n-1) + fib_cache_built_in(n-2)
+
+
+print(fib_cache_built_in(10))
