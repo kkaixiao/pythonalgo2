a7a1a4fd01ac8dd3866527697652616a9d7fdfe0
kkaixiao <show-key@qq.com>
2019-12-13 16:22:14 +0800

    1st commit without watching tutorial.

---
+++ Pascal_Triangle.py
@@ -0,0 +1,28 @@
+
+def pascal_triangle(n):
+    if n < 0:
+        return []
+
+    res = [[1], [1, 1]]
+
+    if n <= 2:
+        return res[:n]
+
+    pre_line = res[1]
+
+    for i in range(n-1):
+
+        current_line = pre_line.copy()
+        current_line.append(1)
+
+        for j in range(1, i+2):
+            current_line[j] = pre_line[j-1] + pre_line[j]
+
+        res.append(current_line)
+
+        pre_line = current_line.copy()
+
+    return res
+
+
+print(pascal_triangle(5))
