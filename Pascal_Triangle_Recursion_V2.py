d31b885c13d7cde7a5cd39612f0c7d4fc25b5610
kkaixiao <show-key@qq.com>
2019-12-14 13:28:13 +0800

    1st commit without watching tutorial.

---
+++ Pascal_Triangle_Recursion_V2.py
@@ -0,0 +1,24 @@
+def pascal_triangle(n, current_row=0, prev=[]):
+
+    # base case #1, if recursion finishes, the function should return an empty array
+    if current_row > n:
+        return []
+    # start recursion from here, given current_row is by default 0
+    elif current_row == 0:
+        return pascal_triangle(n, current_row+1, [1])
+
+    # start to do other recursion when current_row goes up, remember, variable prev will be updated after each recursion
+    # it is calculated by the calculate_pascal_triangle_row function
+    else:
+        return [prev] + pascal_triangle(n, current_row+1, calculate_pascal_triangle_row(prev))
+
+
+# let's calculate items for each recursion
+def calculate_pascal_triangle_row(prev):
+    res = [1] * (len(prev)+1)
+    for i in range(1, len(prev)):
+        res[i] = prev[i-1] + prev[i]
+    return res
+
+
+print(pascal_triangle(1))
