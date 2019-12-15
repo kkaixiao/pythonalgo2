5bd346c67aa643182c40a0ee109f4e17a9437369
kkaixiao <show-key@qq.com>
2019-12-15 12:31:41 +0800

    2nd commit, applied 4 approaches, without watching tutorial.

--- single_number.py
+++ single_number.py
@@ -24,21 +24,52 @@ def single_num_xor(nums):
 
 
 def single_num_index(nums):
+    if not nums:
+        return 0
+    if len(nums) == 1:
+        return nums[0]
+
     while len(nums) > 0:
         try:
-            if nums[1:].index(nums[0]) >= 0:
-                nums.pop(nums[1:].index(nums[0])+1)
+            tmp_ndx = nums.index(nums[0], 1, len(nums))
+            if tmp_ndx >= 0:
+
+                nums.pop(tmp_ndx)
                 nums.pop(0)
+
         except ValueError:
             return nums[0]
 
+
+
+def single_num_set(nums):
+    print(set(nums))
+    return sum(nums) - sum(set(nums))
+
+
+
+def single_num_recursion(nums):
+
+    if len(nums) == 0:
+        return 0
+    if len(nums) == 1:
+        return nums[0]
+    try:
+        nums.pop(nums.index(nums[0], 1, len(nums)))
+    except ValueError:
+        return nums[0]
+    nums.pop(0)
+    return single_num_recursion(nums)
 
 
 arr1 = [2,5,2,5,1]
 arr2 = [4,1,2,1,2]
 arr3 = [2,5,2,8,5,8,9]
 arr4 = [2, 2, 1]
+arr5 = []
 
 
 # print(single_num_xor(arr2))
-print(single_num_index(arr4))
+# print(single_num_index(arr4))
+# print(single_num_set(arr3))
+print(single_num_recursion(arr3))
