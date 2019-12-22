bfbe989628bcc7c6ab1d406b5b2d96dac6d36934
kkaixiao <show-key@qq.com>
2019-12-22 23:04:01 +0800

    1st commit

---
+++ udemy_ds_alg_interview_108.py
@@ -0,0 +1,12 @@
+def BinaryTree(r):
+    return [r, [], []]
+
+
+def insert_left(root, new_branch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1, [new_branch, t, []])
+    else:
+        root.insert(1, [new_branch, [], []])
+    return root
+
