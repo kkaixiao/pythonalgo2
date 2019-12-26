e4e056c8dd9c1abbc204dc4c33bc5b6484d58859
kkaixiao <show-key@qq.com>
2019-12-26 11:47:15 +0800

    1st, nothing done yet.

---
+++ beat_codility_039_number_of_disc_intersections.py
@@ -0,0 +1,9 @@
+'''
+for problem definition, check out the following url:
+https://app.codility.com/programmers/task/number_of_disc_intersections/
+
+'''
+
+dict = {2:(-5, 4), 1:(-5,3)}
+a = sorted(dict)
+print(a)
