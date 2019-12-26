572641825680da14a23cd20904d9a9a7351b74f5
kkaixiao <show-key@qq.com>
2019-12-26 11:54:37 +0800

    1st, included lambda, decorator and genertor

---
+++ aaa_just_useful_tutorial_links.py
@@ -0,0 +1,9 @@
+
+# nice thing on lambda function
+print('lambda function tutorial:', 'https://realpython.com/python-lambda/')
+
+# nice thing on decorator
+print('decorator:', 'https://realpython.com/primer-on-python-decorators/')
+
+# generator
+print('generator', 'https://realpython.com/introduction-to-python-generators/')
