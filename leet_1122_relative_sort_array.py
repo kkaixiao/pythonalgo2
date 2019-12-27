218e92570e5ff2732c069c5ff5ea86a5ae6de79d
kkaixiao <show-key@qq.com>
2019-12-27 15:16:17 +0800

    2nd commit, made a second further enumerator for creating the matched numbers

--- leet_1122_relative_sort_array.py
+++ leet_1122_relative_sort_array.py
@@ -16,9 +16,24 @@ def relative_sort_array(arr1, arr2):
     return sorted_stack+sorted(unsorted_stack)
 
 
+def relative_sort_array2(arr1, arr2):
+    sorted_stack = []
+    unsorted_stack =[]
+
+    for item2 in arr2:
+        found_indices = [i for i, x in enumerate(arr1) if x == item2]
+        found_stack = [y for i, y in enumerate(arr1) if i in found_indices]
+        sorted_stack.extend(found_stack)
+
+    for item in arr1:
+        if item not in sorted_stack:
+            unsorted_stack.append(item)
+
+    return sorted_stack+sorted(unsorted_stack)
+
 
 array1 = [2,3,1,3,2,4,6,7,9,2,19]
 array2 = [2,1,4,3,9,6]
 
 
-print(relative_sort_array(array1, array2))
+print(relative_sort_array2(array1, array2))
