88b5302ff4eb3c219e9199e3389f82691ca2476f
kkaixiao <show-key@qq.com>
2019-12-27 15:32:37 +0800

    3rd commit, added two more implementations

--- leet_1122_relative_sort_array.py
+++ leet_1122_relative_sort_array.py
@@ -32,8 +32,27 @@ def relative_sort_array2(arr1, arr2):
     return sorted_stack+sorted(unsorted_stack)
 
 
+# this is coded by Ricky, super smart and efficient
+def relative_sort_array3(arr1, arr2):
+    res = []
+    for each in arr2:           # iterate all items in arr2
+        while (each in arr1):   # as long as the item in arr2 can be found in arr1
+            res.append(each)    # add the item to the end of the list to be returned
+
+            arr1.remove(each)  # and immediately remove the item in arr1, therefore in the end of the for loop,
+            #                    arr1 will only contains items not found in arr2
+    return res+sorted(arr1)
+
+
+# this is an extremely concise and efficient pythonic code for this question
+def relative_sort_array4(arr1, arr2):
+    order = {v: i for i, v in enumerate(arr2)}  # create a dictionary to be used for sorting key, or can be called a
+    #                                             sorting pattern
+
+    return sorted(arr1, key=lambda a: order.get(a, 1000 + a))   # with the sorting key, arr1 can be ordered by the key
+
 array1 = [2,3,1,3,2,4,6,7,9,2,19]
 array2 = [2,1,4,3,9,6]
 
 
-print(relative_sort_array2(array1, array2))
+print(relative_sort_array4(array1, array2))
