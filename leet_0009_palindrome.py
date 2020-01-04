cbcc62529b72fed2b255c862a12a369a51a530c6
kkaixiao <show-key@qq.com>
2020-01-04 10:01:22 +0800

    2nd commit, added a int with array version

---
+++ leet_0009_palindrome.py
@@ -0,0 +1,84 @@
+"""
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same
+backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore
+ it is not a palindrome.
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+"""
+
+def palindrome(num):
+    if num < 0:
+        return False
+    if num < 10:
+        return True
+    half_len = len(str(num))//2
+    len_mod = len(str(num)) % 2
+    return str(num)[:half_len] == reverse_chars(str(num)[half_len+len_mod:])
+
+
+def reverse_chars(chars):
+    res = ''
+    for i in range(len(chars)):
+        res += chars[-i-1]
+    return res
+
+
+def palindrome1(num):
+    if num < 0:
+        return False
+    if num < 10:
+        return True
+    half_len = len(str(num))//2
+    len_mod = len(str(num)) % 2
+    return str(num)[:half_len] == reverse_chars(str(num)[half_len+len_mod:])
+
+
+def palindrome_direct_int(num):
+    if 0 <= num <= 9:
+        return True
+    if num < 0 or not(num % 10):
+        return False
+    return num == reverse_int(num)
+
+
+def reverse_int(num):
+    reverse = 0
+    while num > 0:
+        rem = num % 10
+        reverse = reverse * 10 + rem
+        num = int(num / 10)
+    return reverse
+
+
+def palindrome_num_array(num):
+    if 0 <= num <= 9:
+        return True
+    if num < 0 or not(num % 10):
+        return False
+    digits = []
+    while num > 0:
+        digits.append(num % 10)
+        num = num // 10
+
+    for i in range(len(digits)//2):
+        if digits[i] != digits[-i-1]:
+            return False
+
+    return True
+
+
+print(palindrome_num_array(334))
+
