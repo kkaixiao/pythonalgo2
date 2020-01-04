f3978385b7e6163a5b4fde67bb6989621e68bdc4
kkaixiao <show-key@qq.com>
2020-01-04 19:31:45 +0800

    1st commit, a simple O(N) method

--- leet_0067_add_binary.py
+++ leet_0067_add_binary.py
@@ -13,6 +13,7 @@ Input: a = "1010", b = "1011"
 Output: "10101"
 """
 
+
 def add_binary(chars1, chars2):
     if len(chars1) > len(chars2):
         list1, list2 = list(chars1), list(chars2)
@@ -27,14 +28,13 @@ def add_binary(chars1, chars2):
         is_carry = sum_bits_and_is_carry // 2
 
     for i in range(len(list1) - len(list2)-1, -1, -1):
-        # print(list1[i])
+
         sum_bits_and_is_carry = int(list1[i]) + is_carry
         res.insert(0, sum_bits_and_is_carry % 2)
         is_carry = sum_bits_and_is_carry // 2
 
     if is_carry == 1:
         res.insert(0, is_carry)
-
 
     res_str = [None]*len(res)
     for i in range(len(res)):
