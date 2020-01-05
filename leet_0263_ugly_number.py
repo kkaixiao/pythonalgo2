b7e4b9867926b13db36bdf30409f29b5a64ed794
kkaixiao <show-key@qq.com>
2020-01-05 21:03:30 +0800

    1st commit, made one sub-function to examine if a number can be divided by ugly primes.

---
+++ leet_0263_ugly_number.py
@@ -0,0 +1,52 @@
+"""
+Write a program to check whether a given number is an ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+
+Example 1:
+
+Input: 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: 8
+Output: true
+Explanation: 8 = 2 × 2 × 2
+Example 3:
+
+Input: 14
+Output: false
+Explanation: 14 is not ugly since it includes another prime factor 7.
+Note:
+
+1 is typically treated as an ugly number.
+Input is within the 32-bit signed integer range: [−2^31,  2^31 − 1].
+"""
+
+def ugly_number(num):
+    if num > 2**31-1 or num < -(2*31) or num == 0:
+        return False
+    if num == 1:
+        return True
+    while num != 1:
+        ugly_result = ugly_dividable(num)
+        if ugly_result > 1:
+            num //= ugly_result
+        else:
+            return False
+
+    return True
+
+def ugly_dividable(num):
+    ugly_primes = [2, 3, 5]
+    for item in ugly_primes:
+        if num % item == 0:
+            return item
+    return -1
+
+
+# print(ugly_dividable(11))
+
+print(ugly_number(6))
+
