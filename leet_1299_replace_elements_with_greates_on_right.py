06b47414004bc842a48102d5099f5b2ae10754de
kkaixiao <show-key@qq.com>
2020-01-06 21:28:50 +0800

    2nd commit, added one more implementation done by others, which is of sapce complexity of O(1) and very concise

--- leet_1299_replace_elements_with_greates_on_right.py
+++ leet_1299_replace_elements_with_greates_on_right.py
@@ -21,19 +21,22 @@ Constraints:
 def replace_elements(ints):
     if len(ints) == 1:
         return [-1]
-    res = [None] * len(ints)
-
-    res[-1] = -1
-
+    res = [-1] * len(ints)
     temp_max = ints[-1]
     for i in range(len(ints) - 2, 0, -1):
         temp_max = max(ints[i + 1], temp_max)
 
-        res[i - 1] = max(temp_max, ints[i])
+        res[i - 1] = max(ints[i + 1], ints[i])
 
     res[-2] = ints[-1]
     return res
 
 
+def replace_elements2(ints):
+    last_val = -1
+    for val in range(len(ints) - 1, -1, -1):
+        ints[val], last_val = last_val, max(ints[val], last_val)
+    return ints
+
 arr1 = [17,18,5,4,6,1]
-print(replace_elements(arr1))
+print(replace_elements2(arr1))
