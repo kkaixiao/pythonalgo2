9dc15381a9a44c2343daca1c7d40518ca6c01f42
kkaixiao <show-key@qq.com>
2020-01-08 10:59:25 +0800

    1st commit, a traditional method, but not so fast

--- leet_0013_roman_to_integer.py
+++ leet_0013_roman_to_integer.py
@@ -50,10 +50,10 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 def roman_to_int(chars):
     dict_roman= {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = prev = dict_roman[chars[0]]
+
     for i in range(1, len(chars)):
         if prev >= dict_roman[chars[i]]:
             total = total + dict_roman[chars[i]]
-
         else:
             total = total + dict_roman[chars[i]] - 2*prev
         prev = dict_roman[chars[i]]
