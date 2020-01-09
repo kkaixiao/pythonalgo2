4d0c3750e25b4c6a4f3147ae1abce8e09aa1ccfa
kkaixiao <show-key@qq.com>
2020-01-09 22:02:54 +0800

    3rd commit, added a function with kmp algorithm

--- leet_0028_implment_strStr_function.py
+++ leet_0028_implment_strStr_function.py
@@ -40,9 +40,75 @@ def strStr2(haystack, needle):
     return haystack.find(needle)
 
 
-# haystack1 = 'hello'
-# needle1 = 'll'
-haystack1="mississippi"
-needle1="pi"
+def strStr_kmp(haystack, needle):
 
-print(strStr2(haystack1, needle1))
+    m = len(haystack)
+    n = len(needle)
+
+    # create lps[] that will hold the longest prefix suffix
+    # values for pattern
+    lps = [0] * n
+    j = 0  # index for pat[]
+
+    # Preprocess the pattern (calculate lps[] array)
+    compute_lps_array(needle, n, lps)
+
+    i = 0  # index for txt[]
+    while i < m:
+        if needle[j] == haystack[i]:
+            i += 1
+            j += 1
+
+        if j == n:
+            print("Found pattern at index " + str(i - j))
+            return i - j
+            # j = lps[j - 1]
+
+            # mismatch after j matches
+        elif i < m and needle[j] != haystack[i]:
+            # Do not match lps[0..lps[j-1]] characters,
+            # they will match anyway
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+    return -1
+
+
+def compute_lps_array(needle, n, lps):
+    len_long_prev_suffix = 0  # length of the previous longest prefix suffix
+
+    lps[0] = 0  # lps[0] is always 0
+    i = 1
+
+    # the loop calculates lps[i] for i = 1 to M-1
+    while i < n:
+        if needle[i] == needle[len_long_prev_suffix]:
+            len_long_prev_suffix += 1
+            lps[i] = len_long_prev_suffix
+            i += 1
+        else:
+            # This is tricky. Consider the example.
+            # AAACAAAA and i = 7. The idea is similar
+            # to search step.
+            if len_long_prev_suffix != 0:
+                len_long_prev_suffix = lps[len_long_prev_suffix - 1]
+
+                # Also, note that we do not increment i here
+            else:
+                lps[i] = 0
+                i += 1
+
+
+haystack1 = "ABABDABACDABABCABAB"
+needle1 = "ABABCABAB"
+print(strStr_kmp(haystack1, needle1))
+
+
+
+# # haystack1 = 'hello'
+# # needle1 = 'll'
+# haystack1="mississippi"
+# needle1="pi"
+#
+# print(strStr2(haystack1, needle1))
