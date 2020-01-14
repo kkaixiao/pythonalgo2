7c059aff403fb85010937a63d448a0dcf661136d
kkaixiao <show-key@qq.com>
2020-01-14 22:18:10 +0800

    2nd commit, added two more funcs with or without converting integer into string, and with lambda function

--- leet_1281_subtract_product_and_sum.py
+++ leet_1281_subtract_product_and_sum.py
@@ -19,7 +19,7 @@ Product of digits = 4 * 4 * 2 * 1 = 32
 Sum of digits = 4 + 4 + 2 + 1 = 11
 Result = 32 - 11 = 21
 """
-from math import log10, floor
+from math import log10
 
 def subtract_product_and_sum(num):
     product_num = 1
@@ -34,8 +34,7 @@ def subtract_product_and_sum2(num):
     product_num = 1
     sum_num = 0
 
-    len_num = int(log10(num))
-    for i in range(len_num+1):
+    for i in range(int(log10(num))+1):
         product_num *= num//(10**i)%10
         sum_num += num//(10**i)%10
     return product_num - sum_num
@@ -43,13 +42,17 @@ def subtract_product_and_sum2(num):
 
 from functools import reduce
 
+
 def subtract_product_and_sum3(num):
     return reduce(lambda x, y: x*y, [int(x) for x in str(num)]) - \
            reduce(lambda x, y: x+y, [int(x) for x in str(num)])
 
 
+def subtract_product_and_sum4(num):
+    return reduce(lambda x, y: x*y, [num//(10**x)%10 for x in range(int(log10(num))+1)]) - \
+           reduce(lambda x, y: x+y, [num//(10**x)%10 for x in range(int(log10(num))+1)])
 
 
 # print(subtract_product_and_sum(4567801))
 
-print(subtract_product_and_sum3(1234))
+print(subtract_product_and_sum4(234))
