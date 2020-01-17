eccece13cf03869746245817010fb606028a0424
kkaixiao <show-key@qq.com>
2020-01-17 23:23:55 +0800

    2nd commit, converting diagonal distance with x and y distance comparison

--- leet_1266_min_time_visitinga_all_points.py
+++ leet_1266_min_time_visitinga_all_points.py
@@ -34,9 +34,19 @@ points[i].length == 2
 
 
 class Solution:
-    # def minTimeToVisitAllPoints(self, points):
 
     def minTimeToVisitAllPoints(self, points):
+        cost = 0
+        for i in range(1, len(points)):
+            start_point, next_point = points[i-1], points[i]
+            cost += max(abs(start_point[0]-next_point[0]), abs(start_point[1]-next_point[1]))
+
+        return cost
+
+
+
+    # a bad code, not good at all cases, failed in leetcode
+    def minTimeToVisitAllPoints2(self, points):
         start_point = points[0]
         seconds = 0
 
@@ -72,7 +82,7 @@ class Solution:
         return seconds
 
 
-    def find_diagonal_line_point(self, point_start, point_destination):
+    def find_diagonal_line_point2(self, point_start, point_destination):
         # we need to go diagonally to a point where either x-coordinate or y-coordinate is the same as
         # that of point_destination.
         # in this approach, we should go to a point whose distances to point_start along x-axis and y-axis
@@ -86,16 +96,25 @@ class Solution:
         diagonal_x = point_destination[1] - point_start[1] + point_start[0]
         diagonal_y = point_destination[0] - point_start[0] + point_start[1]
 
-        if abs(diagonal_x - point_start[0]) > abs(diagonal_y - point_start[1]):
+        if abs(diagonal_x - point_start[0]) >= abs(diagonal_y - point_start[1]):
             return [point_destination[0], diagonal_y]
         else:
             return [diagonal_x, point_destination[1]]
 
+    def find_diagonal_line_point(self, point_start, point_destination):
 
-points1 = [[1,1],[3,4],[-1,0]]
-# points1 = [[3,2],[-2,2]]
+        distance_from_diag_x = abs(point_destination[0] - point_start[0])
+        distance_from_diag_y = abs(point_destination[1] - point_start[1])
+
+        # if distance_x > distance_y:
+        #     return [point_destination[1], diagonal_y]
+        # else:
+        #     return [diagonal_x, point_destination[1]]
+
+# points1 = [[1,1],[3,4],[-1,0]]
+# points1 = [[3, 2], [-2, 2]]
 #
-# points1 = [[559,511],[932,618],[-623,-443],[431,91],[838,-127],[773,-917],[-500,-910],[830,-417],[-870,73],[-864,-600],[450,535],[-479,-370],[856,573],[-549,369],[529,-462],[-839,-856],[-515,-447],[652,197],[-83,345],[-69,423],[310,-737],[78,-201],[443,958],[-311,988],[-477,30],[-376,-153],[-272,451],[322,-125],[-114,-214],[495,33],[371,-533],[-393,-224],[-405,-633],[-693,297],[504,210],[-427,-231],[315,27],[991,322],[811,-746],[252,373],[-737,-867],[-137,130],[507,380],[100,-638],[-296,700],[341,671],[-944,982],[937,-440],[40,-929],[-334,60],[-722,-92],[-35,-852],[25,-495],[185,671],[149,-452]]
+points1 = [[559,511],[932,618],[-623,-443],[431,91],[838,-127],[773,-917],[-500,-910],[830,-417],[-870,73],[-864,-600],[450,535],[-479,-370],[856,573],[-549,369],[529,-462],[-839,-856],[-515,-447],[652,197],[-83,345],[-69,423],[310,-737],[78,-201],[443,958],[-311,988],[-477,30],[-376,-153],[-272,451],[322,-125],[-114,-214],[495,33],[371,-533],[-393,-224],[-405,-633],[-693,297],[504,210],[-427,-231],[315,27],[991,322],[811,-746],[252,373],[-737,-867],[-137,130],[507,380],[100,-638],[-296,700],[341,671],[-944,982],[937,-440],[40,-929],[-334,60],[-722,-92],[-35,-852],[25,-495],[185,671],[149,-452]]
 this_obj = Solution()
 
 print(this_obj.minTimeToVisitAllPoints(points1))
