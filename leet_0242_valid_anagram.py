2ca7e30b722bb4c5b18ace071e6006805a6c7df5
kkaixiao <show-key@qq.com>
2020-02-11 16:49:53 -0600

    1st commit, the first dict method

---
+++ leet_0242_valid_anagram.py
@@ -0,0 +1,38 @@
+"""
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+"""
+
+class Solution:
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        dict_s = dict()
+        for char in s:
+            dict_s[char] = dict_s.get(char, 0) + 1
+
+        for char in t:
+            dict_s[char] = dict_s.get(char, 0) - 1
+
+        for k, v in dict_s.items():
+            if v > 0:
+                return False
+
+        return True
+
+
+mysol = Solution()
+print(mysol.isAnagram('a','ab'))
