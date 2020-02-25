26188c6d7f5bc4fd17217c060444e8ca64849a78
kkaixiao <show-key@qq.com>
2020-02-24 22:06:51 -0600

    1st commit

---
+++ leet_0155_min_stack.py
@@ -0,0 +1,16 @@
+class MinStack:
+
+    def __init__(self):
+        self.A = [float('inf')]
+        self.min = [float('inf')]
+    def push(self, x: int) -> None:
+        self.A.append(x)
+        self.min.append(min(x,self.min[-1]))
+
+    def pop(self) -> None:
+        self.A.pop()
+        self.min.pop()
+    def top(self) -> int:
+        return self.A[-1]
+    def getMin(self) -> int:
+        return self.min[-1]
