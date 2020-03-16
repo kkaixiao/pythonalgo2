406bdc3858f40b9f0abc6db91a158cdc3fdb88ee
kkaixiao <show-key@qq.com>
2020-03-16 01:22:22 -0600

    1st commit

---
+++ leet_0225_implement_stack_using_queues    .py
@@ -0,0 +1,63 @@
+"""
+Implement the following operations of a stack using queues.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+empty() -- Return whether the stack is empty.
+Example:
+
+MyStack stack = new MyStack();
+
+stack.push(1);
+stack.push(2);
+stack.top();   // returns 2
+stack.pop();   // returns 2
+stack.empty(); // returns false
+"""
+
+from collections import deque
+
+
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        if len(self.q1) == 0:
+            self.q1.appendleft(x)
+            return
+
+        while self.q1:
+            self.q2.append(self.q1.popleft())
+
+        self.q1.appendleft(x)
+
+        while self.q2:
+            self.q1.append(self.q2.popleft())
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.q1.popleft()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.q1[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return len(self.q1) == 0
