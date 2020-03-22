30aab5b30ea350cd7b2a5d128c42a6ce59938cb4
kkaixiao <show-key@qq.com>
2020-03-22 16:52:08 -0600

    1st commit

---
+++ leet_0237_delete_node_in_a_linked_list.py
@@ -0,0 +1,54 @@
+"""
+Write a function to delete a node (except the tail) in a singly linked list, given only access to
+that node.
+
+Given linked list -- head = [4,5,1,9], which looks like following:
+
+
+
+Example 1:
+
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9
+after calling your function.
+Example 2:
+
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9
+after calling your function.
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        while node.next.next is not None:
+            node.val = node.next.val
+            node = node.next
+        node.val = node.next.val
+        node.next = None
+
+    def deleteNode2(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        while node.next:
+            if not node.next.next:
+                nxt = node.next
+                node.next = None
+                node.val = nxt.val
+                break
+            node.val = node.next.val
+            node = node.next
