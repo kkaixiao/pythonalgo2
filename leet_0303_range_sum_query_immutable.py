744f3564bbe0da3bc9957eae62c9850336c0c85a
kkaixiao <show-key@qq.com>
2020-03-30 00:30:26 -0600

    1st commit

---
+++ leet_0303_range_sum_query_immutable.py
@@ -0,0 +1,32 @@
+"""
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j),
+inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+"""
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
+
+class NumArray:
+
+    def __init__(self, nums):
+        self.arrays = []
+        total = 0
+
+        for number in nums:
+            total += number
+            self.arrays.append(total)
+
+    def sumRange(self, i, j):
+        if i == 0:
+            return self.arrays[j]
+        else:
+            return self.arrays[j] - self.arrays[i - 1]
+
