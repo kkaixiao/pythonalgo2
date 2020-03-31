a6fcdcad16b978e5b3203ebf83f14bbb749343b3
kkaixiao <show-key@qq.com>
2020-03-31 02:02:21 -0600

    1st commit

--- leet_0326_power_of_three.py
+++ leet_0326_power_of_three.py
@@ -32,3 +32,17 @@ class Solution:
             return True
         return self.isPowerOfThree(n // 3)
 
+
+    def isPowerOfThree_ite(self, n: int) -> bool:
+        while n > 1:
+            n = n/3
+        return n == 1
+
+
+    def isPowerOfThree_O1(self, n: int) -> bool:
+        if n < 1:
+            return False
+
+        res = math.log10(n) / math.log10(3)
+
+        return math.floor(res) == math.ceil(res)
