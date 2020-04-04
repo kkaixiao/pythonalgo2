83d886ca2c9315a4496c90a9f7220ec21ead5fcf
kkaixiao <show-key@qq.com>
2020-04-04 02:53:23 -0600

    1st commit

---
+++ leet_0374_guess_number_high_or_low.py
@@ -0,0 +1,35 @@
+"""
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+Example :
+
+Input: n = 10, pick = 6
+Output: 6
+"""
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        start, end = 1, n
+        while start <= end:
+            middle = start + (end - start) // 2
+            hint = guess(middle)
+            if hint == 0:
+                return middle
+            elif hint == -1:
+                end = middle - 1
+            else:
+                start = middle + 1
