9454762bedd9339d94064b9a74c4e87e1e35ee66
kkaixiao <show-key@qq.com>
2020-04-12 01:51:15 -0600

    1st commit

--- leet_0409_longest_palindrome.py
+++ leet_0409_longest_palindrome.py
@@ -7,12 +7,11 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         odd = 0
         res = 0
-        sets = set(s)
-        for e in sets:
+        for e in set(s):
             num = s.count(e)
             if num % 2 == 0:
                 res += num
             else:
-                res = res + num -1
+                res = res + num - 1
                 odd = 1
         return res + odd
