da53faf8e6a94a8026824f40f2af5a152626126e
kkaixiao <show-key@qq.com>
2020-04-13 23:01:57 -0600

    1st commit

--- leet_0414_third_maximum_num.py
+++ leet_0414_third_maximum_num.py
@@ -24,6 +24,8 @@ Explanation: Note that the third maximum here means the third maximum distinct n
 Both numbers with value 2 are both considered as second maximum.
 """
 
+# for heap to be used
+import heapq
 
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
@@ -35,4 +37,30 @@ class Solution:
             return nums[len(nums) - 3]
         else:
             return nums[len(nums) - 1]
+
+    # withou sort approach
+    def thirdMax(self, nums: List[int]) -> int:
+        if len(set(nums)) < 3:
+            return max(nums)
+
+        nums = set(nums)
+        nums.remove(max(nums))
+        nums.remove(max(nums))
+
+        return max(nums)
+
+    # use heap
+
+
+
+    def thirdMax(self, nums: List[int]) -> int:
+        heap = []
+        nums = list(set(nums))
+        if len(nums) < 3:
+            return max(nums)
+        for n in nums:
+            heapq.heappush(heap, -n)
+        for i in range(1,3):
+            heapq.heappop(heap)
+        return -heapq.heappop(heap)
 
