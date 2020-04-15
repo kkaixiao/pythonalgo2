44988c76357cb1dac478b129cde76cd0a5eb31f3
kkaixiao <show-key@qq.com>
2020-04-14 23:21:54 -0600

    1st commit

--- leet_0415_add_strings.py
+++ leet_0415_add_strings.py
@@ -35,3 +35,23 @@ class Solution:
         for i in range(lStr):
             res += mapDict[s[i]] * (10 ** (lStr - i - 1))
         return res
+
+
+    # fastest solution with map and bit operation
+    def addStrings(self, num1: str, num2: str) -> str:
+        num1, num2 = list(map(int, num1[::-1])), list(map(int, num2[::-1]))
+
+        if len(num1)<len(num2):
+            num1, num2 = num2, num1
+
+        carry = 0
+        for i in range(len(num1)):
+            n = num2[i] if i<len(num2) else 0
+            tmp = n + carry + num1[i]
+            num1[i] = tmp%10
+            carry = tmp//10
+
+        if carry:
+            num1.append(1)
+
+        return ''.join(map(str, num1))[::-1]
