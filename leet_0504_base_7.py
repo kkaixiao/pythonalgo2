00f79ad1be4456762a3f54d50034a4b542c55d86
kkaixiao <show-key@qq.com>
2020-05-03 23:28:15 -0600

    1st commit

---
+++ leet_0504_base_7.py
@@ -0,0 +1,30 @@
+"""
+Given an integer, return its base 7 string representation.
+
+Example 1:
+Input: 100
+Output: "202"
+Example 2:
+Input: -7
+Output: "-10"
+Note: The input will be in range of [-1e7, 1e7].
+"""
+
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if -7 < num < 7:
+            return str(num)
+        digits = []
+        res = ''
+        if num < 0:
+            res = '-'
+        num = abs(num)
+        while num > 6 or num < -6:
+            digits.append(num % 7)
+            num //= 7
+        digits.append(num)
+
+        for i in range(len(digits) - 1, -1, -1):
+            res += str(digits[i])
+
+        return res
