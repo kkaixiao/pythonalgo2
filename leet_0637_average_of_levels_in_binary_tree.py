b79af36c076ed695140a1fa8e920ad7a8801475f
kkaixiao <show-key@qq.com>
2020-05-27 02:26:52 -0600

    1st commit

--- leet_0637_average_of_levels_in_binary_tree.py
+++ leet_0637_average_of_levels_in_binary_tree.py
@@ -15,3 +15,37 @@ return [3, 14.5, 11].
 Note:
 The range of node's value is in the range of 32-bit signed integer.
 """
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recordEachLevel(self, level, listLevel, node):
+        if node:
+            if len(listLevel) == level:
+
+                listLevel.append([node.val, 1])
+            else:
+                listLevel[level][0] += node.val
+                listLevel[level][1] += 1
+
+            # else:
+            #     listLevel.append([node.val, 1])
+            self.recordEachLevel(level + 1, listLevel, node.left)
+            self.recordEachLevel(level + 1, listLevel, node.right)
+
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        res = []
+        l = []
+        self.recordEachLevel(0, l, root)
+        for item in l:
+            res.append(item[0] / item[1])
+        return res
+
+
+
+
