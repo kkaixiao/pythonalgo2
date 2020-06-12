e21d74f779b020d4ad947afec7d2ebedd0a6b565
kkaixiao <show-key@qq.com>
2020-06-12 17:04:37 -0600

    1st commit

---
+++ leet_0717_1-bit_and_2-bit_characters.py
@@ -0,0 +1,54 @@
+"""
+We have two special characters. The first character can be represented by one bit 0. The second
+character can be represented by two bits (10 or 11).
+
+Now given a string represented by several bits. Return whether the last character must be a one-bit
+character or not. The given string will always end with a zero.
+
+Example 1:
+Input:
+bits = [1, 0, 0]
+Output: True
+Explanation:
+The only way to decode it is two-bit character and one-bit character. So the last character is
+one-bit character.
+Example 2:
+Input:
+bits = [1, 1, 1, 0]
+Output: False
+Explanation:
+The only way to decode it is two-bit character and two-bit character. So the last character is NOT
+one-bit character.
+Note:
+
+1 <= len(bits) <= 1000.
+bits[i] is always 0 or 1.
+Accepted
+"""
+
+
+class Solution:
+
+
+    # pop(0)method
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        while len(bits) > 1:
+            bit = bits.pop(0)
+            if bit:
+                bits.pop(0)
+
+        if not len(bits):
+            return False
+        return not bits[0]
+
+    # del method
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        while len(bits) > 1:
+            bit = bits[0]
+            del bits[0]
+            if bit:
+                del bits[0]
+
+        if not len(bits):
+            return False
+        return not bits[0]
