1bfed0fa61e3fbd65a896ab583b7325de1d9529f
kkaixiao <show-key@qq.com>
2020-06-16 02:41:55 -0600

    1st commit

--- leet_0744_ find_smallest_letter_greater_than_target.py
+++ leet_0744_ find_smallest_letter_greater_than_target.py
@@ -42,6 +42,7 @@ target is a lowercase letter.
 """
 
 class Solution:
+    # function without binary search
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
         if target < letters[0] or target >= letters[-1]:
             return letters[0]
@@ -51,3 +52,19 @@ class Solution:
         for letter in letters:
             if letter > target:
                 return letter
+
+    # binary search function
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        if target < letters[0] or target >= letters[-1]:
+            return letters[0]
+
+        left, right = 0, len(letters) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if letters[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return letters[left]
