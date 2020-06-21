b7e55415ab61c617ce8b7fb2e039bb30a2b05f65
kkaixiao <show-key@qq.com>
2020-06-20 19:18:05 -0600

    1st commit

---
+++ leet_0788_rotated_digits.py
@@ -0,0 +1,43 @@
+"""
+X is a good number if after rotating each digit individually by 180 degrees, we get a valid number
+that is different from X.  Each digit must be rotated - we cannot choose to leave it alone.
+
+A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves;
+2 and 5 rotate to each other (on this case they are rotated in a different direction, in other words
+2 or 5 gets mirrored); 6 and 9 rotate to each other, and the rest of the numbers do not rotate to
+any other number and become invalid.
+
+Now given a positive number N, how many numbers X from 1 to N are good?
+
+Example:
+Input: 10
+Output: 4
+Explanation:
+There are four good numbers in the range [1, 10] : 2, 5, 6, 9.
+Note that 1 and 10 are not good numbers, since they remain unchanged after rotating.
+Note:
+
+N  will be in range [1, 10000].
+"""
+
+
+class Solution:
+    def rotatedDigits(self, N: int) -> int:
+        nogood = [3, 4, 7]
+        good = [2, 5, 6, 9]
+
+        res = 0
+        for i in range(1, N + 1):
+            num = i
+            isgood = 0
+            while num > 0:
+                digit = num % 10
+                if digit in nogood:
+                    isgood = 0
+                    break
+                if digit in good:
+                    isgood += 1
+                num //= 10
+            if isgood > 0:
+                res += 1
+        return res
