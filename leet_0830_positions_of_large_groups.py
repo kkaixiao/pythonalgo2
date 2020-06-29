2eeabaa9f9c1d62ec05750ed254c451beb1ff411
kkaixiao <show-key@qq.com>
2020-06-29 01:16:03 -0600

    1st commit

--- leet_0830_positions_of_large_groups.py
+++ leet_0830_positions_of_large_groups.py
@@ -44,3 +44,15 @@ class Solution:
                 res.append([k, v])
 
         return res
+
+    # a good solution
+    def largeGroupPositions(self, S: str) -> List[List[int]]:
+        ans = []
+        i = 0 # The start of each group
+        for j in range(len(S)):
+            if j == len(S) - 1 or S[j] != S[j+1]:
+                # Here, [i, j] represents a group.
+                if j-i+1 >= 3:
+                    ans.append([i, j])
+                i = j+1
+        return ans
