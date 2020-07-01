938d30b10b81b34c9423648654cd9e32236a899f
kkaixiao <show-key@qq.com>
2020-06-30 23:36:23 -0600

    1st commit

---
+++ leet_0836_rectangle_overlap.py
@@ -0,0 +1,36 @@
+"""
+A rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) are the coordinates of its
+bottom-left corner, and (x2, y2) are the coordinates of its top-right corner.
+
+Two rectangles overlap if the area of their intersection is positive.  To be clear, two rectangles
+that only touch at the corner or edges do not overlap.
+
+Given two (axis-aligned) rectangles, return whether they overlap.
+
+Example 1:
+
+Input: rec1 = [0,0,2,2], rec2 = [1,1,3,3]
+Output: true
+Example 2:
+
+Input: rec1 = [0,0,1,1], rec2 = [1,0,2,1]
+Output: false
+Notes:
+
+Both rectangles rec1 and rec2 are lists of 4 integers.
+All coordinates in rectangles will be between -10^9 and 10^9.
+"""
+
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        # view the first as a triangle, check if the other comes to top, left, bottom or right
+        if rec1[1] >= rec2[3] or rec2[1] >= rec1[3] or rec1[2] <= rec2[0] or rec2[2] <= rec1[0]:
+            return False
+        return True
+
+    # a second solution by checking size of rectangles
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+
+        width = min(rec1[2], rec2[2]) - max(rec1[0], rec2[0])
+        height = min(rec1[3], rec2[3]) - max(rec1[1], rec2[1])
+        return width > 0 and height > 0
