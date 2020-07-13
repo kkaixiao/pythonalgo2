a66b0e47031faabd61248204e33324c1fa8d96aa
kkaixiao <show-key@qq.com>
2020-07-13 01:22:25 -0600

    1st commit

---
+++ questions_plus_minus_questions_shirlene_age_4.py
@@ -0,0 +1,22 @@
+import random
+
+nPlus = 8
+
+while nPlus > 0:
+    num1 = random.randint(0, 20)
+
+    num2 = random.randint(0, 20)
+
+    if num1 + num2 <= 20:
+        print(str(num1) + '  +  ' + str(num2) + ' =')
+        nPlus -= 1
+
+nMinus = 4
+
+while nMinus > 0:
+    num1 = random.randint(0, 20)
+
+    num2 = random.randint(0, 20)
+    if num1 - num2 >= 0:
+        print(str(num1) + '  -  ' + str(num2) + ' =')
+        nMinus -= 1
