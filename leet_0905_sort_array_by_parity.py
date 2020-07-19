5940d19302c77b7a10d16c8a6f2845fcdc82a885
kkaixiao <show-key@qq.com>
2020-07-19 01:53:22 -0600

    1st commit

--- leet_0905_sort_array_by_parity.py
+++ leet_0905_sort_array_by_parity.py
@@ -47,3 +47,12 @@ class Solution:
             A[left], A[right] = A[right], A[left]
 
         return A
+
+    # the third method without combining lists in the end
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        j = 0
+        for i in range(len(A)):
+            if A[i]%2==0:
+                A[i], A[j] = A[j], A[i]
+                j+=1
+        return A
