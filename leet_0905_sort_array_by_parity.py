c2fe1b82fec2a97e9c84165c5459fb23934478ca
kkaixiao <show-key@qq.com>
2020-07-19 01:30:44 -0600

    1st commit

--- leet_0905_sort_array_by_parity.py
+++ leet_0905_sort_array_by_parity.py
@@ -30,3 +30,20 @@ class Solution:
         even.extend(odd)
 
         return even
+
+    # binary search approach, looks cool, but not so fast
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+
+        left, right = 0, len(A) - 1
+
+        while left < right:
+
+            while A[left] % 2 == 0 and left < right:
+                left += 1
+
+            while A[right] % 2 == 1 and left < right:
+                right -= 1
+
+            A[left], A[right] = A[right], A[left]
+
+        return A
