a1a9551795743a76a609dcd3758cab1b469a39e0
kkaixiao <show-key@qq.com>
2020-07-23 00:08:56 -0600

    1st commit

---
+++ leet_0806_number_of_lines_to_write_string.py
@@ -0,0 +1,41 @@
+"""
+
+"""
+
+
+class Solution:
+    # with the use of dictionary
+    def numberOfLines(self, widths: List[int], S: str) -> List[int]:
+        d = dict()
+        for i in range(len(widths)):
+            d[chr(i + 97)] = widths[i]
+
+        line, count = 1, 0
+
+        for c in S:
+            if count + d[c] <= 100:
+                count += d[c]
+            else:
+                line += 1
+                count = d[c]
+
+        return [line, count]
+
+    # without the use of dictionary
+    def numberOfLines(self, widths: List[int], S: str) -> List[int]:
+        line, count = 1, 0
+        units = 100
+
+        for letter in S:
+            index = ord(letter) - 97
+            difference = units - widths[index]
+
+            if (difference < 0):
+                line += 1
+                units = 100 - widths[index]
+            else:
+                units = difference
+
+        count = 100 - units
+
+        return [line, count]
