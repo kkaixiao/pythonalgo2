4dd5346cdcd44151d3c3d76d7bc2c598470326b5
kkaixiao <show-key@qq.com>
2020-09-04 03:34:38 -0600

    1st commit

---
+++ leet_1175_prime_arrangements.py
@@ -0,0 +1,47 @@
+"""
+Return the number of permutations of 1 to n so that prime numbers are at prime indices (1-indexed.)
+
+(Recall that an integer is prime if and only if it is greater than 1, and cannot be written as a
+product of two positive integers both smaller than it.)
+
+Since the answer may be large, return the answer modulo 10^9 + 7.
+
+
+
+Example 1:
+
+Input: n = 5
+Output: 12
+Explanation: For example [1,2,5,4,3] is a valid permutation, but [5,2,3,4,1] is not because the prime
+number 5 is at index 1.
+Example 2:
+
+Input: n = 100
+Output: 682289015
+
+
+Constraints:
+
+1 <= n <= 100
+"""
+
+
+class Solution:
+    def numPrimeArrangements(self, n: int) -> int:
+        def isPrime(num):
+            if num <= 1:
+                return False
+            if num <= 3:
+                return True
+            for i in range(2, num):
+                if num % i == 0:
+                    return False
+            return True
+
+        primeCount = 0
+
+        for i in range(1, n + 1):
+            if isPrime(i):
+                primeCount += 1
+
+        return math.factorial(primeCount) * math.factorial(n - primeCount) % (10 ** 9 + 7)
