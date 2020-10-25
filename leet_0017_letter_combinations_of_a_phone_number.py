c28c032162c96934497cd5fecbd01a9d23f1cb73
kkaixiao <show-key@qq.com>
2020-10-25 00:04:46 -0600

    1st commit

--- leet_0017_letter_combinations_of_a_phone_number.py
+++ leet_0017_letter_combinations_of_a_phone_number.py
@@ -57,3 +57,23 @@ class Solution(object):
 
         return prod
 
+
+# 2nd solution without using itertools
+class Solution(object):
+    def letterCombinations(self, digits):
+        res = []
+        d_map = {'2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'], '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']}
+        for i, c in enumerate(digits):
+            if i == 0:
+                for d in d_map[c]:
+                    res.append(d)
+            else:
+                newRes = []
+                for d in d_map[c]:
+                    for s in res:
+                        newRes.append(''.join([s, d]))
+
+                res = newRes
+        return res
