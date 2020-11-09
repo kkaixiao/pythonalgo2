d4b3bb935510b49b79e7f7f8f8a1704bc0ecf766
kkaixiao <show-key@qq.com>
2020-11-08 23:40:23 -0600

    1st commit

--- leet_0020_valid_parentheses.py
+++ leet_0020_valid_parentheses.py
@@ -1,28 +1,56 @@
-def is_valid(s):
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if
+the input string is valid.
 
-    if len(s)%2 != 0:
-        return False
+An input string is valid if:
 
-    opening = set('([{')
-    matches = set([ ('(', ')'), ('[', ']'), ('{', '}') ])
-
-    stack = []
-
-    for paren in s:
-        if paren in opening:
-            stack.append(paren)
-        else:
-            if len(stack) == 0:
-                return False
-            last_open = stack.pop()
-            if (last_open, paren) not in matches:
-                return False
-    return len(stack) == 0
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
 
 
-print(is_valid('{}'))
-print(is_valid('{()}'))
-print(is_valid('{(})'))
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+Example 4:
+
+Input: s = "([)]"
+Output: false
+Example 5:
+
+Input: s = "{[]}"
+Output: true
+"""
+
+class Solution(object):
+    def isValid(self, s):
+
+        if len(s)%2 != 0:
+            return False
+
+        opening = set('([{')
+        matches = set([ ('(', ')'), ('[', ']'), ('{', '}') ])
+
+        stack = []
+
+        for paren in s:
+            if paren in opening:
+                stack.append(paren)
+            else:
+                if len(stack) == 0:
+                    return False
+                last_open = stack.pop()
+                if (last_open, paren) not in matches:
+                    return False
+        return len(stack) == 0
 
 
 
